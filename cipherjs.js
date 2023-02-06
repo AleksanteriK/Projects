6f87977e8e2e3490a7e62a89d088b38bcb4b9e18
AleksanteriK <120490294+AleksanteriK@users.noreply.github.com>
2023-02-06 13:56:34 +0200

    Now i got finally the script to work! The site is finally complete and I'm satisfied with the results.

--- cipherjs.js
+++ cipherjs.js
@@ -1,1 +1,34 @@
-//Nothing here yet, previous script did not work and was not understandable, going to make a whole new script for the site.
+let alphabet='ABCDEFGHIJKLMNOPQRSTUVWXYZ';
+
+function Cipher () {
+    let text=document.getElementById("entext").value;
+    let newtext="";
+    let shift=Number(document.getElementById("enchar").value);
+    
+        for (let i=0;i<text.length;i++){
+            let pos=alphabet.indexOf(text[i].toUpperCase());
+            let newpos=(pos+shift) %26;
+            newtext+=alphabet.charAt(newpos);
+        }
+
+    document.getElementById("result").innerHTML=newtext;
+}
+
+function Decipher () {
+    let text=document.getElementById("detext").value;
+    let newtext="";
+    let shift=Number(document.getElementById("dechar").value);
+    
+        for (let i=0;i<text.length;i++){
+            let pos=alphabet.indexOf(text[i].toUpperCase());
+            let newpos=(pos-shift) %26;
+
+            if (newpos<0) {
+                newpos=newpos+26;
+            }
+            
+            newtext+=alphabet.charAt(newpos);
+        }
+
+    document.getElementById("result").innerHTML=newtext;
+}
